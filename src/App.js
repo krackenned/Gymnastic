b16514025e659383bf0c86df525567f367e854d8
krackenned <103532751+krackenned@users.noreply.github.com>
2022-06-30 01:27:09 -0300

    borrando <Text>

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import LoginForm from "./components/LoginForm";
 export default function App() {
   return (
     <View>
-      <Text>Soy un text en app.js</Text>
       <LoginForm />
     </View>
   );
