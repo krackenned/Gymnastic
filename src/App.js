67ad52f109eeafa274abf740576a301a0a63989a
krackenned <103532751+krackenned@users.noreply.github.com>
2022-06-30 01:55:05 -0300

    agregando informacion.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
 import { View, Text } from "react-native";
 import LoginForm from "./components/LoginForm";
+import Informacion from "./components/Informacion";
 
 export default function App() {
   return (
     <View>
-      <LoginForm />
+      <div class="container">
+        <div class="pantalla_info">
+          <Informacion />
+        </div>
+
+        <Text> </Text>
+
+        <div class="pantalla_loginform">
+          <LoginForm />
+        </div>
+      </div>
     </View>
   );
 }
---
+++ src/components/Informacion.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function LoginForm() {
+  return (
+    <View>
+      <Text>Que eventos se realizan?</Text>
+      <Text>Se realizan eventos de todo tipo menos ferias.</Text>
+      <Text>Que dias y horarios?</Text>
+      <Text>Solo feriados y fines de semana.</Text>
+      <Text>Metodos de pago:</Text>
+      <Text>
+        Las instalaciones se prestan a cambio de productos de limpieza.
+      </Text>
+      <Text>Condiciones de alquiler:</Text>
+      <Text>
+        Cumplir con la limpieza. No romper ni perder elementos, de ser asi
+        hacerse responsable.
+      </Text>
+    </View>
+  );
+}
