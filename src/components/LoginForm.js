1d43ea33a56c3761d3d3a67cb28572f5a0553971
krackenned <103532751+krackenned@users.noreply.github.com>
2022-06-30 01:40:59 -0300

    agregando <TextInput> de datos personales

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,11 +1,17 @@
 import React from "react";
-import { View, Text, TextInput, Button } from "react-native";
+import { View, TextInput, Button } from "react-native";
 
 export default function LoginForm() {
   return (
     <View>
+      <TextInput placeholder="Nombre" />
+      <TextInput placeholder="Apellido" />
+      <TextInput placeholder="DNI" />
+      <TextInput placeholder="Edad" />
+      <TextInput placeholder="Domicilio" />
       <TextInput placeholder="Email" />
-      <TextInput placeholder="Password" />
+      <TextInput placeholder="Sexo" />
+      <Button title="Cancelar" onPress={() => console.log("Cancelado")} />
       <Button title="Enviar" onPress={() => console.log("Enviado")} />
     </View>
   );
