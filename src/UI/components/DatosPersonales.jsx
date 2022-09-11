f8d049725ebf1c53a9e71080df6813f01a89dcc5
Krackenned <103532751+krackenned@users.noreply.github.com>
2022-09-11 17:56:51 -0300

    arreglando-import-material-ui

--- src/UI/components/DatosPersonales.jsx
+++ src/UI/components/DatosPersonales.jsx
@@ -1,52 +1,20 @@
 import React from "react";
-import {View} from "react-native"
-import { TextField,Button } from "@mui/material";
+import { View } from "react-native";
+import { TextField, Button } from "@material-ui/core";
 
-export default function DatosPersonales(){
-    return(
-        <View>
-            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-            <TextField
-            id="outlined-nombre"
-            type="nombre"
-            label="Nombre"
-
-            />
-            <TextField
-            id="outlined-apellido"
-            type="apellido"
-            label="Apellido"
-            />
-             <TextField
-            id="outlined-dni"
-            type="Dni"
-            label="Dni"
-            />
-             <TextField
-            id="outlined-edad"
-            type="edad"
-            label="Edad"
-            />
-             <TextField
-            id="outlined-domicilio"
-            type="domicilio"
-            label="Domicilio"
-            />
-             <TextField
-            id="outlined-email"
-            type="email"
-            label="Email"
-            />
-            <TextField
-            id="outlined-genero"
-            type="genero"
-            label="Genero"
-            />
-            <Button variant="contained">Enviar</Button>
-            <Button variant="contained">Cancelar</Button>
-            
-
-        </View>
-
-    )
+export default function DatosPersonales() {
+  return (
+    <View>
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      <TextField id="outlined-nombre" type="nombre" label="Nombre" />
+      <TextField id="outlined-apellido" type="apellido" label="Apellido" />
+      <TextField id="outlined-dni" type="Dni" label="Dni" />
+      <TextField id="outlined-edad" type="edad" label="Edad" />
+      <TextField id="outlined-domicilio" type="domicilio" label="Domicilio" />
+      <TextField id="outlined-email" type="email" label="Email" />
+      <TextField id="outlined-genero" type="genero" label="Genero" />
+      <Button variant="contained">Enviar</Button>
+      <Button variant="contained">Cancelar</Button>
+    </View>
+  );
 }
